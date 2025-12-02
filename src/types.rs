88929cdbfd8e2b32ef7f4e5193d9019364fb3c4d
Samuel Atkins <s.m.atkins73@gmail.com>
2025-12-02 10:25:02 -0600

    Fix OpenOrder deserialization - remove incorrect type rename

--- src/types.rs
+++ src/types.rs
@@ -966,7 +966,6 @@ pub struct OpenOrder {
     pub asset_id: String,
     #[serde(deserialize_with = "crate::decode::deserializers::number_from_string")]
     pub expiration: u64,
-    #[serde(rename = "type")]
     pub order_type: OrderType,
     #[serde(deserialize_with = "crate::decode::deserializers::number_from_string")]
     pub created_at: u64,
